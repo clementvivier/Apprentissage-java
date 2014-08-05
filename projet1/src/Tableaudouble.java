96917f9485e465925a21da81a1a6ef176ed04050
bill0ute <bill0ute@users.noreply.github.com>
2014-08-05 15:05:55 +0800

    Petites corrections (plus de details dans la description)
    Tout d'abord, une des conventions de Java est que le nom de chaque classe commence par une majuscule. De plus, le nom du fichier contenant la classe doit avoir exactement le meme nom que celle ci.
    
    Ensuite, niveau indentation, voila a quoi ressemble mon code. C'est personnel (et j'imagine pas le meme que ce que tu as dans le bouquin).
    Le plus important c'est clairement de pouvoir te relire.
    J'ai pris l'habitute notamment de mettre l'accolade ouvrante (pour la classe, une fonction, une boucle, un if, etc) a la ligne, et au niveau de l'instruction. Et d'ensuite rajouter une tabulation pour tout ce qui est entre accolade.
    Je mets aussi pas mal d'espace, comme tu peux le voir. Java ne prend pas en compte les espaces/retour a la ligne/tabulation. Donc tu peux en mettre un peu ou tu veux !
    
    Continue comme ca :)

---
+++ projet1/src/Tableaudouble.java
@@ -0,0 +1,31 @@
+
+public class Tableaudouble
+{
+
+	/**
+	 * @param args
+	 */
+	public static void  main (String  args [])
+	{
+		// TODO Auto-generated method stub
+		int  premiersNombres [][] = {{0, 2, 4, 6, 8}, {1, 3, 5, 7, 9}};
+		int  i = 0;
+		int  j = 0;
+		
+		while (i < 2)
+		{
+			j = 0;
+			
+			while (j < 5)
+			{
+				System.out.print (premiersNombres [i][j]);
+				j++;
+			}
+			
+			System.out.println ("");
+			i++;
+		}
+	}
+	
+	
+}
